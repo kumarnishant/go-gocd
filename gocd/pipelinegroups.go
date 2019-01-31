5a996e645f29dee33e54179e82e5ade7d17bd547
nishant <nishant@devtron.ai>
2019-01-31 19:19:31 +0530

    corrected url

--- gocd/pipelinegroups.go
+++ gocd/pipelinegroups.go
@@ -41,11 +41,11 @@ func (pgs *PipelineGroupsService) List(ctx context.Context, name string) (*Pipel
 func (pgs *PipelineGroupsService) Create(ctx context.Context, name string) (*PipelineGroup, *APIResponse, error) {
 	pg := &PipelineGroup{}
 	_, resp, err := pgs.client.postAction(ctx, &APIClientRequest{
-		Path:         "config/pipeline_groups",
-		ResponseType: responseTypeJSON,
+		Path:         "admin/pipeline_groups",
 		RequestBody:  PipelineGroup{Name: name},
 		ResponseBody: pg,
 		APIVersion:   apiV1,
+		ResponseType: responseTypeJSON,
 	})
 
 	return pg, resp, err
