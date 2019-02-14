49e1bd8c3d87bad87594eae7db07ba3d2f0a1776
nishant <nishant@devtron.ai>
2019-02-14 13:18:06 +0530

    added artifact store config

---
+++ gocd/artifact_store.go
@@ -0,0 +1,27 @@
+package gocd
+
+import "context"
+
+type Property struct {
+	Key            string `json:"key"`
+	Value          string `json:"value,omitempty"`
+	EncryptedValue string `json:"encrypted_value,omitempty"`
+}
+type ArtifactStore struct {
+	Links      *HALLinks   `json:"_links,omitempty"`
+	PluginId   string      `json:"plugin_id"`
+	properties []*Property `json:"properties"`
+}
+
+type ArtifactStoreService service
+
+func (crs *ArtifactStoreService) Create(ctx context.Context, store *ArtifactStore) (out *ArtifactStore, resp *APIResponse, err error) {
+	out = &ArtifactStore{}
+	_, resp, err = crs.client.postAction(ctx, &APIClientRequest{
+		Path:         "admin/config_repos",
+		RequestBody:  store,
+		ResponseBody: out,
+		APIVersion:   apiV1,
+	})
+	return
+}
