57a7cad785567ea4e7c40d49dcaa99003b2879ff
Vikram Singh <vikram@devtron.ai>
2019-05-13 14:20:32 +0530

    Update, Get, Get ALL method edit for ArtifactStore

--- gocd/artifact_store.go
+++ gocd/artifact_store.go
@@ -26,3 +26,42 @@ func (crs *ArtifactStoreService) Create(ctx context.Context, store *ArtifactStor
 	})
 	return
 }
+
+func (crs *ArtifactStoreService) Update(ctx context.Context, store *ArtifactStore, eTag string) (out *ArtifactStore, resp *APIResponse, err error) {
+	out = &ArtifactStore{}
+	request := &APIClientRequest{
+		Path:         "admin/artifact_stores/" + store.ID,
+		APIVersion:   apiV1,
+		RequestBody:  store,
+		ResponseBody: out,
+		Headers:      map[string]string{"If-Match": eTag},
+	}
+	_, resp, err = crs.client.putAction(ctx, request)
+	return
+}
+
+func (crs *ArtifactStoreService) Get(ctx context.Context, storeId string) (store *ArtifactStore, resp *APIResponse, err error) {
+	//var contentResponse string
+	store = &ArtifactStore{}
+	_, resp, err = crs.client.getAction(ctx, &APIClientRequest{
+		Path:         "admin/artifact_stores/" + storeId,
+		ResponseBody: store,
+		//ResponseType: responseTypeText,
+		APIVersion: apiV1,
+	})
+
+	return
+}
+
+func (crs *ArtifactStoreService) GetAll(ctx context.Context) (stores []ArtifactStore, resp *APIResponse, err error) {
+	//var contentResponse string
+	//var stores []ArtifactStore
+	_, resp, err = crs.client.getAction(ctx, &APIClientRequest{
+		Path:         "admin/artifact_stores",
+		ResponseBody: &stores,
+		ResponseType: responseTypeText,
+		APIVersion:   apiV1,
+	})
+
+	return
+}
